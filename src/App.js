aba83919fbcd1c64c1e8948211756f24201e19b7
Ralfs Fadejevs <ralphfdesigns@gmail.com>
2024-05-05 19:30:49 +0200

    updated file structure

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Weather from './Weather';
+import Education from './Education';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Farmer Education SaaS</h1>
+      {/* <Weather />
+      <Education /> */}
+    </div>
+  );
+};
+
+export default App;
